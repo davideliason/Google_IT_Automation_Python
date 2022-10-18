e4e2dbf9d62301032a555ae7c7388232db1b9dcb
David Eliason <davide@Davids-MacBook-Air.local>
2022-10-18 14:04:10 -0700

    script using os to test given directory and then iterate through each object and testing to see if a if a directory or not

--- Python_and_OS/iterate_through_folder_using_os_test_object_type.py
+++ Python_and_OS/iterate_through_folder_using_os_test_object_type.py
@@ -5,7 +5,11 @@ import os
 dir = "test_dir"
 
 for object in os.listdir(dir):
-	# here, just print out what's inside directory
-	print("{} is in the folder".format(object))
+    # here, just print out what's inside directory
+    print("{} is in the folder".format(object))
+    fullname = os.path.join(dir,object)
+    if os.path.isdir(fullname):
+        print("{} is a directory".format(fullname))
+    else:
+        print("{} is a file".format(fullname))
 
-print("done")
