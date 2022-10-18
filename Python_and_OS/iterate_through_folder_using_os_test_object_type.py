e8adad91c241f865ad682dde9c7f537daee97f59
David Eliason <davide@Davids-MacBook-Air.local>
2022-10-18 14:07:51 -0700

    add explanatory text

--- Python_and_OS/iterate_through_folder_using_os_test_object_type.py
+++ Python_and_OS/iterate_through_folder_using_os_test_object_type.py
@@ -7,6 +7,8 @@ dir = "test_dir"
 for object in os.listdir(dir):
     # here, just print out what's inside directory
     print("{} is in the folder".format(object))
+
+    # now, iterate through given directory to test objects
     fullname = os.path.join(dir,object)
     if os.path.isdir(fullname):
         print("{} is a directory".format(fullname))
